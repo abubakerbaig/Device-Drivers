560212ce8ebb3bf110e1c4e436de1f3d7be5c95e
Mirza Abubaker <abubaker.mirza11@gmail.com>
2020-12-01 09:45:31 -0800

    Work Queue demo

---
+++ workq/.sample.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/abubakerbaig/device_drivers/workq/sample.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000  --build-id  -T ./scripts/module-common.lds -o /home/abubakerbaig/device_drivers/workq/sample.ko /home/abubakerbaig/device_drivers/workq/sample.o /home/abubakerbaig/device_drivers/workq/sample.mod.o;  true
---
+++ workq/.sample.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/abubakerbaig/device_drivers/workq/sample.mod := { echo  /home/abubakerbaig/device_drivers/workq/sample.o; echo; } > /home/abubakerbaig/device_drivers/workq/sample.mod
